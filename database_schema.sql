012dcc0b364ad860656425db831f262e026e43f6
Stephen Pinto <spinto7@hotmail.com>
2022-09-25 17:16:11 -0400

    Added boards and task to dabase schema. Added CRUD operations for boards. Updated endpoints to better comply with REST best practices. Added async error handling.

--- database_schema.sql
+++ database_schema.sql
@@ -5,3 +5,22 @@ CREATE TABLE account (
 	password VARCHAR (60) NOT NULL,
 	created_date TIMESTAMPTZ NOT NULL
 );
+
+CREATE TABLE board (
+	id serial PRIMARY KEY,
+	owner_id integer references account(id) NOT NULL,
+	title VARCHAR(20) NOT NULL,
+	created_date TIMESTAMPTZ NOT NULL,
+	last_updated TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE task (
+	id serial PRIMARY KEY,
+	author_id integer references account(id) NOT NULL,
+	assignee_id integer references account(id),
+	state VARCHAR(4) NOT NULL, -- TODO, WIP, DONE
+	title VARCHAR(30) NOT NULL,
+	description VARCHAR(500) NOT NULL,
+	created_date TIMESTAMPTZ NOT NULL,
+	last_updated TIMESTAMPTZ NOT NULL
+);
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const { createClient } = require('redis');
 const RedisStore = require('connect-redis')(session);
 const helmet = require('helmet');
+const asyncHandler = require('express-async-handler');
 
 const app = express();
 const port = 3000;
@@ -44,8 +45,7 @@ function isAuthenticated(req, res, next) {
   if (req.session && req.session.username && req.session.username.length > 0) {
     next();
   } else {
-    res.status(401);
-    res.json({ error: 'User is not logged in' });
+    res.status(401).json({ error: 'User is not logged in' });
   }
 }
 
@@ -75,87 +75,205 @@ async function getUser(username) {
   return result.rows[0];
 }
 
-app.get('/api/username', isAuthenticated, async (req, res) => {
-  res.json({ result: req.session.username });
+app.get(
+  '/api/username',
+  isAuthenticated,
+  asyncHandler(async (req, res) => {
+    res.json(req.session.username);
+  })
+);
+
+app.post(
+  '/api/register',
+  asyncHandler(async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username) {
+      res.status(400).json({ error: 'No username supplied' });
+      return;
+    } else if (typeof username !== 'string') {
+      res.status(400).json({ error: 'Username must be a string' });
+      return;
+    } else if (username.length > 20) {
+      res.status(400).json({ error: 'Username must be 20 characters or less' });
+      return;
+    } else if (!password) {
+      res.status(400).json({ error: 'No password supplied' });
+      return;
+    } else if (typeof password !== 'string') {
+      res.status(400).json({ error: 'Password must be a string' });
+      return;
+    } else if (password.length > 120) {
+      res
+        .status(400)
+        .json({ error: 'Password must be 120 characters or less' });
+      return;
+    }
+
+    if (await getUser(username)) {
+      res.status(400).json({ error: 'Username already exists' });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(12);
+    const passwordHash = bcrypt.hashSync(password, salt);
+
+    const insertResult = await pool.query(
+      'INSERT INTO account (username, password, salt, created_date) VALUES ($1, $2, $3, NOW())',
+      [username, passwordHash, salt]
+    );
+
+    if (insertResult.rowCount === 0) {
+      res.status(500).json({ error: 'Failed to create new user' });
+      return;
+    }
+
+    res.status(204).json();
+  })
+);
+
+app.post(
+  '/api/login',
+  asyncHandler(async (req, res) => {
+    const { username: suppliedUsername, password: suppliedPassword } = req.body;
+    if (!suppliedUsername) {
+      res.status(400).json({ error: 'No username supplied' });
+      return;
+    } else if (typeof suppliedUsername !== 'string') {
+      res.status(400).json({ error: 'Username must be a string' });
+      return;
+    } else if (!suppliedPassword) {
+      res.status(400).json({ error: 'No password supplied' });
+      return;
+    } else if (typeof suppliedPassword !== 'string') {
+      res.status(400).json({ error: 'Password must be a string' });
+      return;
+    }
+
+    const user = await getUser(suppliedUsername);
+    if (!user) {
+      res.status(400);
+      res.json({ error: 'Username is not registered' });
+      return;
+    }
+
+    if (!bcrypt.compareSync(suppliedPassword, user.password)) {
+      res.status(400);
+      res.json({ error: 'Password was incorrect' });
+      return;
+    }
+
+    req.session.regenerate(() => {
+      req.session.username = user.username;
+      req.session.userId = user.id;
+      res.status(204).json();
+    });
+  })
+);
+
+app.post('/api/logout', (req, res) => {
+  req.session.destroy(() => res.status(204).json());
 });
 
-app.post('/api/register', async (req, res) => {
-  const { username, password } = req.body;
+app
+  .route('/api/boards')
+  .get(
+    isAuthenticated,
+    asyncHandler(async (req, res) => {
+      const result = await pool.query(
+        'SELECT * FROM board WHERE owner_id = $1',
+        [req.session.userId]
+      );
 
-  if (!username) {
-    res.status(400);
-    res.json({ error: 'No username supplied' });
-    return;
-  } else if (username.length > 20) {
-    res.status(400);
-    res.json({ error: 'Username must be 20 characters or less' });
-    return;
-  } else if (!password) {
-    res.status(400);
-    res.json({ error: 'No password supplied' });
-    return;
-  } else if (password.length > 120) {
-    res.status(400);
-    res.json({ error: 'Password must be 120 characters or less' });
-    return;
-  }
+      res.json({ result: result.rows });
+    })
+  )
+  .post(
+    isAuthenticated,
+    asyncHandler(async (req, res) => {
+      const { title } = req.body;
 
-  if (await getUser(username)) {
-    res.status(400);
-    res.json({ error: 'Username already exists' });
-    return;
-  }
+      if (!title) {
+        res.status(400).json({ error: 'No title supplied' });
+        return;
+      } else if (typeof title !== 'string') {
+        res.status(400).json({ error: 'Title must be a string' });
+      }
 
-  const salt = bcrypt.genSaltSync(12);
-  const passwordHash = bcrypt.hashSync(password, salt);
+      const client = await pool.connect();
+      try {
+        const createResult = await client.query(
+          'INSERT INTO board (owner_id, title, created_date, last_updated) VALUES ($1, $2, NOW(), NOW()) returning id',
+          [req.session.userId, title]
+        );
 
-  const insertResult = await pool.query(
-    'INSERT INTO account (username, password, salt, created_date) VALUES ($1, $2, $3, NOW())',
-    [username, passwordHash, salt]
+        if (createResult.rowCount === 0) {
+          res.status(500).json({ error: 'Failed to create new board' });
+          return;
+        }
+
+        const newBoardResult = await client.query(
+          'SELECT * FROM board WHERE id = $1 AND owner_id = $2',
+          [createResult.rows[0].id, req.session.userId]
+        );
+
+        return res.status(201).json(newBoardResult.rows[0]);
+      } catch (error) {
+        throw error;
+      } finally {
+        client.release();
+      }
+    })
   );
 
-  if (insertResult.rowCount === 0) {
-    res.status(500);
-    res.json({ error: 'Failed to create new user' });
-    return;
-  }
+app
+  .route('/api/boards/:boardId')
+  .get(
+    isAuthenticated,
+    asyncHandler(async (req, res) => {
+      const { boardId } = req.params;
 
-  res.json({ message: 'Registration success' });
-});
+      if (!boardId) {
+        res.status(400).json({ error: 'No board id supplied' });
+        return;
+      }
 
-app.post('/api/login', async (req, res) => {
-  const { username: suppliedUsername, password: suppliedPassword } = req.body;
-  if (!suppliedUsername) {
-    res.status(400);
-    res.json({ error: 'No username supplied' });
-    return;
-  } else if (!suppliedPassword) {
-    res.status(400);
-    res.json({ error: 'No password supplied' });
-    return;
-  }
+      const result = await pool.query(
+        'SELECT * FROM board WHERE id = $1 AND owner_id = $2',
+        [boardId, req.session.userId]
+      );
 
-  const user = await getUser(suppliedUsername);
-  if (!user) {
-    res.status(400);
-    res.json({ error: 'Username is not registered' });
-    return;
-  }
+      if (result.rows.length === 0) {
+        res.status(404).json({ error: 'Unable to find board' });
+        return;
+      }
 
-  if (!bcrypt.compareSync(suppliedPassword, user.password)) {
-    res.status(400);
-    res.json({ error: 'Password was incorrect' });
-    return;
-  }
+      res.json({ result: result.rows[0] });
+    })
+  )
+  .delete(
+    isAuthenticated,
+    asyncHandler(async (req, res) => {
+      const { boardId } = req.params;
 
-  req.session.username = user.username;
-  res.json({ message: 'Login success' });
-});
+      if (!boardId) {
+        res.status(400).json({ error: 'No board id supplied' });
+        return;
+      }
 
-app.get('/api/logout', (req, res) => {
-  req.session.destroy();
-  res.json({ message: 'Logout success' });
-});
+      const result = await pool.query(
+        'DELETE FROM board WHERE owner_id = $1 AND id = $2',
+        [req.session.userId, boardId]
+      );
+
+      if (result.rowCount === 0) {
+        res.status(500).json({ error: 'Unable to delete board' });
+        return;
+      }
+
+      res.status(204).json();
+    })
+  );
 
 // custom 404
 app.use((req, res, next) => {
